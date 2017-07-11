076b19c26ea6488cd998ab330691225bd05d9c56
Lubos Michalic <lnmichalic@gmail.com>
2017-07-11 15:11:41 +0100

    Refactor attr_reader methods into individual methods

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,20 @@
 # Transaction class
 class Transaction
-  attr_reader :amount, :time, :type
   def initialize(amount, time, type)
     @amount = amount
     @time = time
     @type = type
+  end
+
+  def check_amount
+    @amount
+  end
+
+  def give_pretty_time
+    @time.strftime('%Y/%m/%d')
+  end
+
+  def check_type
+    @type
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,8 +4,10 @@ describe Transaction do
   subject(:deposit) { described_class.new(300, time = Time.now, :debit)}
 
   it 'has an amount, type, and time when instantiated' do
-    expect(deposit.amount).to eq 300
-    expect(deposit.time.strftime("%Y/%m/%d")).to eq '2017/07/11'
-    expect(deposit.type).to eq :debit
+    expect(deposit.check_amount).to eq 300
+    expect(deposit.give_pretty_time).to eq '2017/07/11'
+    expect(deposit.check_type).to eq :debit
   end
+
+
 end
